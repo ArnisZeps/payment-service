736ee732e8bf6b1dafa427e0d21aa2cfc09f0c00
Arnis Zeps <zeps.arnis@gmail.com>
2026-07-20 22:40:59 +0300

    feat/payments-router

---
+++ src/routes/payment.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from "express";
+
+export function createPaymentRoutes(): Router {
+    const router = Router();
+
+    // post payment route
+    // get payments route
+
+    return router;
+}
